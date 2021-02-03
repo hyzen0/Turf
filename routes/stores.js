baf7000bceb230b8f7a0ef119c90d82b4d58f8bf
hyzen0 <hail4heisenberg@gmail.com>
2021-02-03 19:02:38 +0530

    push

--- routes/stores.js
+++ routes/stores.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const { getStores, addStore } = require("../controllers/stores");
-
+const bodyParser = require("body-parser");
 const router = express.Router();
+const intersect = require();
+router.use(bodyParser.urlencoded({ extended: true }));
 
-router.route("/").get(getStores).post(addStore);
+router.post("/post-test", (req, res) => {
+  console.log("Got body:", req.body);
+  var line1 = turf.lineString([]);
+
+  var line2 = turf.lineString([
+    [-74.0386542, 40.7302174],
+    [-74.038756, 40.7295611],
+  ]);
+  var intersects = turf.lineIntersect(line1, line2);
+  console.log(intersects);
+
+  res.sendStatus(200);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require("./config/db");
 
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
-connectDB();
 
 const app = express();
 
@@ -22,7 +20,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use("/api/v1/stores", require("./routes/stores"));
+app.use("/api", require("./routes/stores"));
 
 const PORT = process.env.PORT || 5000;
 
